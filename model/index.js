1747d76b37e4bc29328150b38f7c7ede6439a0a7
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-06-12 00:03:08 +0300

    Added basic logic with local db

--- model/index.js
+++ model/index.js
@@ -1,20 +1,124 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs').promises
+const path = require('path')
 
-const listContacts = async () => {}
+const { customAlphabet } = require('nanoid')
+const newId = customAlphabet('1234567890', 10)
 
-const getContactById = async (contactId) => {}
+const contactsFile = path.join(__dirname, 'contacts.json')
 
-const removeContact = async (contactId) => {}
+// Читает файл со всеми контактами
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsFile, 'utf8')
 
-const addContact = async (body) => {}
+    return JSON.parse(data)
+  } catch (error) {
+    console.error(error.message)
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+// Находит в файле контакт по id
+const getContactById = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsFile, 'utf8')
+    const parsedContacts = JSON.parse(data)
+
+    if (parsedContacts.some(contact => contact.id === Number(contactId))) {
+      const contact = parsedContacts.find(
+        contact => contact.id === Number(contactId)
+      )
+
+      return contact
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+// Создает в файле новый контакт
+const addContact = async (body) => {
+  const { name, email, phone } = body // ДОБАВИТЬ ВАЛИДАЦИЮ!!!
+
+  const newContact = {
+    id: Number(newId()),
+    name,
+    email,
+    phone
+  }
+
+  try {
+    const data = await fs.readFile(contactsFile, 'utf8')
+    const parsedContacts = JSON.parse(data)
+
+    const newContacts = [...parsedContacts, newContact]
+
+    await fs.writeFile(contactsFile, JSON.stringify(newContacts, null, 2))
+
+    return newContact
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+// Удаляет контакт из файла
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactsFile, 'utf8')
+    const parsedContacts = JSON.parse(data)
+
+    // Ищет в массиве хотя бы одно совпадение по id. Если есть, тогда фильтрует массив
+    if (parsedContacts.some(contact => contact.id === Number(contactId))) {
+      const filteredContacts = parsedContacts.filter(
+        contact => contact.id !== Number(contactId)
+      )
+
+      await fs.writeFile(contactsFile, JSON.stringify(filteredContacts, null, 2))
+
+      return filteredContacts
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
+}
+
+// Обновляет контакт - ДОБАВИТЬ ВАЛИДАЦИЮ!!!
+const updateContact = async (contactId, body) => {
+  try {
+    const data = await fs.readFile(contactsFile, 'utf8')
+    const parsedContacts = JSON.parse(data)
+
+    // Находит нужный контакт по id
+    const neededСontact = parsedContacts.find(
+      contact => contact.id === Number(contactId)
+    )
+
+    // Если контакт есть, тогда обновляем его
+    if (neededСontact) {
+      const updatedСontact = {
+        ...neededСontact,
+        ...body
+      }
+
+      // Проходимся по массиву контактов, если id совпадает, тогда возвращаем обновленный контакт (или старый)
+      const result = parsedContacts.map(contact => {
+        if (contact.id === Number(contactId)) {
+          return updatedСontact
+        } else return contact
+      })
+
+      await fs.writeFile(contactsFile, JSON.stringify(result, null, 2))
+
+      return updatedСontact
+    }
+  } catch (error) {
+    console.error(error.message)
+  }
+}
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
+  removeContact,
   updateContact,
 }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,77 @@
 const express = require('express')
 const router = express.Router()
 
+const { listContacts, getContactById, addContact, removeContact, updateContact } = require('../../model')
+
+// Роут для списка всех контактов
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await listContacts()
+
+    res.status(200).json({ contacts, status: 'success' })
+  } catch (error) {
+    next(error)
+  }
 })
 
+// Роут для контакта по id
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId: id } = req.params
+
+  try {
+    const contact = await getContactById(id)
+
+    if (!contact) {
+      return res.status(404).json({ message: 'Not found' })
+    }
+
+    res.status(200).json({ contact, status: 'success' })
+  } catch (error) {
+    next(error)
+  }
 })
 
+// Роут для создания контакта
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contact = await addContact(req.body)
+
+    res.status(201).json({ contact, status: 'success' })
+  } catch (error) {
+    next(error)
+  }
 })
 
+// Роут для удаления контакта
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const { contactId: id } = req.params
+
+  try {
+    const result = await removeContact(id)
+
+    if (!result) {
+      return res.status(404).json({ message: 'Not found' })
+    }
+
+    res.status(200).json({ message: 'contact deleted' })
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+// Роут для обновления контакта
+router.put('/:contactId', async (req, res, next) => {
+  try {
+    const contact = await updateContact(req.params.contactId, req.body)
+
+    if (!contact) {
+      return res.status(404).json({ message: 'Not found' })
+    }
+
+    res.status(200).json({ contact, status: 'success' })
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
