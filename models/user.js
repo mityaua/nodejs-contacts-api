7551f0b9fa65c24b6b76687abc13cb7a9fd34b3d
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-06-27 17:17:00 +0300

    Added public folder, updated user schema, added gravatar for user

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const gravatar = require('gravatar')
 
 const { Schema } = mongoose
 const { SubTypes } = require('../helpers/constants')
@@ -22,6 +23,12 @@ const userSchema = new Schema({
   token: {
     type: String,
     default: null,
+  },
+  avatarURL: {
+    type: String,
+    default: function () {
+      return gravatar.url(this.email, { s: '250' }, true)
+    },
   },
 })
 
